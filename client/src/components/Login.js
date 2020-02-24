0351c349263317d3ecf2b0437e9dc4b83c8b4be4
Wangdi Tenzing <wangdi457@gmail.com>
2020-02-24 17:20:47 -0500

    Added some styles

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,55 +1,65 @@
-import React, {useState} from "react";
-import axiosWithAuth from "./Auth"
+import React, { useState } from "react";
+import axiosWithAuth from "./Auth";
+import donut from "./Images/donut.svg";
 
 const Login = props => {
- // make a post request to retrieve a token from the api
+  // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  const [credentials,setCredentials]=useState({
-    username:"",
-    password:""
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
   });
-  
-  const login = event=>{
+
+  const login = event => {
     event.preventDefault();
     axiosWithAuth()
-    .post("login", credentials)
-    .then(response=>{
-      console.log("resposne:",response)
-      localStorage.setItem("token", response.data.payload);
-      props.history.push("/bubbles") 
-    })
-    .catch(error=>console.log(error))
-  }
+      .post("login", credentials)
+      .then(response => {
+        console.log("resposne:", response);
+        localStorage.setItem("token", response.data.payload);
+        props.history.push("/bubbles");
+      })
+      .catch(error => console.log(error));
+  };
 
-  const handleChange = event=>{
+  const handleChange = event => {
     setCredentials({
       ...credentials,
-      [event.target.name] :event.target.value
-    })
-  }
+      [event.target.name]: event.target.value
+    });
+  };
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-      <form>
-        <input 
-          type="text"
-          name="username"
-          placeholder=" Username "
-          value={credentials.username}
-          onChange={handleChange}
-        />
-         <input 
-          type="text"
-          name="password"
-          placeholder=" Password "
-          value={credentials.password}
-          onChange={handleChange}
-        />
-        <button onClick={login}>Login</button>
-      </form>
-    </>
+    
+      <div className="titlepage">
+       <h1>React Bubbles</h1>
+       <img src={donut}/>
+       
+        
+      
+        <div className="form">
+        <form>
+          <input
+            type="text"
+            name="username"
+            placeholder=" Username "
+            value={credentials.username}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="password"
+            placeholder=" Password "
+            value={credentials.password}
+            onChange={handleChange}
+          />
+          <button className="login-button" onClick={login}>Login</button>
+        </form>
+        </div>
+       
+        </div>
+      
+    
   );
 };
 
