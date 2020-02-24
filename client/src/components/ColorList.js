7b71d1912bcaf1a3caa0f8eeaa27b38cbe84bc94
Wangdi Tenzing <wangdi457@gmail.com>
2020-02-24 01:02:20 -0500

    MVP DONE

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "./Auth"
 
 const initialColor = {
   color: "",
@@ -14,6 +14,7 @@ const ColorList = ({ colors, updateColors }) => {
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
+    console.log("color," , color)
   };
 
   const saveEdit = e => {
@@ -21,10 +22,32 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
-  };
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(() => {
+        axiosWithAuth()
+          .get(`http://localhost:5000/api/colors/`)
+          .then(response => updateColors(response.data))
+          .catch(error => console.log(error));
+        setEditing(false);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };	  
+  
+
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+    .delete(`http://localhost:5000/api/colors/${color.id}`)
+    .then(()=>{
+      axiosWithAuth()
+      .get("http://localhost:5000/api/colors")
+      .then(response=>updateColors(response.data))
+      .catch(error=>console.log(error))
+    })
   };
 
   return (
@@ -84,6 +107,7 @@ const ColorList = ({ colors, updateColors }) => {
       {/* stretch - build another form here to add a color */}
     </div>
   );
-};
+            };
 
 export default ColorList;
+            
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+import axiosWithAuth from "./Auth"
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
+const Login = props => {
+ // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [credentials,setCredentials]=useState({
+    username:"",
+    password:""
+  });
+  
+  const login = event=>{
+    event.preventDefault();
+    axiosWithAuth()
+    .post("login", credentials)
+    .then(response=>{
+      console.log("resposne:",response)
+      localStorage.setItem("token", response.data.payload);
+      props.history.push("/bubbles") 
+    })
+    .catch(error=>console.log(error))
+  }
+
+  const handleChange = event=>{
+    setCredentials({
+      ...credentials,
+      [event.target.name] :event.target.value
+    })
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form>
+        <input 
+          type="text"
+          name="username"
+          placeholder=" Username "
+          value={credentials.username}
+          onChange={handleChange}
+        />
+         <input 
+          type="text"
+          name="password"
+          placeholder=" Password "
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button onClick={login}>Login</button>
+      </form>
     </>
   );
 };
